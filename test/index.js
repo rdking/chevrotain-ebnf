f970e65670722524f47937fa991031b56f559a39
Ranando King <jisark5@MacBook06.local>
2019-01-25 06:51:05 -0600

    Done, but the generated list for the lexer needs refining...

--- test/index.js
+++ test/index.js
@@ -6,26 +6,24 @@ const EBNF = String.raw
 SubExpression = ( "(", [ ws ], Expression, [ ws ], ")" );
 ws = { " " | "\t" | "\n" | "\r" };
 Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
-Integer = [ ws ], Digit, { Digit };
-Operation = ASValue, { ASOperation };
-ASValue = MDOperation | MDValue;
-MDValue = SubExpression | Integer;
-ASOperation = [ ws ], ASOP, [ ws ], ASValue;
+Integer = Digit, { Digit };
+Operation = ASValue, [ ws ], { ASOperation };
+ASValue = MDValue, [ ws ], [ MDOperation ];
+MDValue = Integer | SubExpression;
+ASOperation = ASOP, [ ws ], ASValue;
 ASOP = Addition | Subtraction;
 Addition = "+";
 Subtraction = "-";
-MDOperation = MDValue, [ ws ], MDOP, [ ws ], MDValue;
+MDOperation = MDOP, [ ws ], MDValue;
 MDOP = Multiplication | Division;
 Multiplication = "*";
 Division = "/";
 `;
 
 var ebnfParser = new EBNFParser(EBNF);
-var parser = ebnfParser.learnLanguage();
-parser.saveSyntaxDiagram("./EBNF.html");
+var Parser = ebnfParser.learnLanguage();
 
-/* The real language examination begins here.
-   
-   let ast = parser.parse(sourceFile);
+const SOURCE = "1 + 2 * 3 - 4 / 5 + 6 / 7 - 8 * 9";
+var parser = new Parser(SOURCE);
+parser.saveSyntaxDiagram("./TestLanguage.html");
 
- */
